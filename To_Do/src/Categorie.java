6eb69073dcdcaff75e3886d7a1ad0c4fa68eaf86
Licinius <florent.dellomo@gmail.com>
2016-11-19 15:11:18 +0100

    Catégorie /* constructeur, Setter, getter et toString*/

--- To_Do/src/Categorie.java
+++ To_Do/src/Categorie.java
@@ -1,4 +1,38 @@
 
 public class Categorie {
+	private int identifiant;
+	private String nom;
 
+	/**
+	 * Constructeur d'une catégorie avec un nom
+	 * @param nom
+	 */
+	public Categorie(String nom) {
+		this.nom = nom;
+	}
+	
+	/**
+	 * Recuperation du nom de la Categorie
+	 * @return String : this.nom 
+	 */
+	public String getNom() {
+		return nom;
+	}
+	
+	/**
+	 * Modifier le nom de la catégorie par le nom passé en paramètre
+	 * @param nom
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	/**
+	 * ToString d'une catégorie
+	 */
+	public String toString() {
+		return "Categorie [identifiant=" + identifiant + ", nom=" + nom + "]";
+	}
+
+	
 }
