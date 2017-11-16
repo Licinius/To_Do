de94161a442bcf01787fce687598de0d5f648136
Licinius <florent.dellomo@gmail.com>
2017-11-16 11:29:21 +0100

    Petit test pour git : Rajout d'un commentaire simple

--- To_Do/src/exception_To_Do/ExceptionTache.java
+++ To_Do/src/exception_To_Do/ExceptionTache.java
@@ -1,5 +1,7 @@
 package exception_To_Do;
-
+/**
+ Classe qui permet de génerer des exceptions de type ExceptionTache
+**/
 public class ExceptionTache extends Exception{
 	private static final long serialVersionUID = 4445839753876631340L;
 	public ExceptionTache(String str){
